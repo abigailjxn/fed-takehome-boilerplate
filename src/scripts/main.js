9f44174312f58d926cdb71b520fedbab1e833fab
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-05-09 13:17:15 -0500

    working mobile nav toggle - need to fix positioning

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,15 @@
 // Put JS logic (if any) here.
 // ESNext to ES5 transpiling is completed via Babel's "env" preset.
+
+const mobileNav = document.getElementsByClassName("mobile-navbar");
+
+function toggleMobileNav(x) {
+    x.classList.toggle("change");
+  }
+
+  function classToggle() {
+    const navs = document.querySelectorAll('.mobile-navbar')
+    navs.forEach(nav => nav.classList.toggle('show-mobile-navbar'));
+  }
+  document.querySelector('.navicon-toggle')
+    .addEventListener('click', classToggle);
